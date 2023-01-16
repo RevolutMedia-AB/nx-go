c4594b1cc49eeb6488e6f3f30e96188448d9b2dd
Gustav Weslien <gustav.weslien@gmail.com>
2023-01-16 14:16:18 +0100

    add cwd to executor.ts

--- packages/nx-go/src/executors/build/executor.ts
+++ packages/nx-go/src/executors/build/executor.ts
@@ -3,8 +3,9 @@ import { runGoCommand } from '../../utils'
 import { BuildExecutorSchema } from './schema'
 
 export default async function runExecutor(options: BuildExecutorSchema, context: ExecutorContext) {
-  const mainFile = `${options.main}`
-  const output = `-o ${options.outputPath}${process.platform === 'win32' ? '.exe' : ''}`
+  const mainFile  = `${options.main}`
+  const output    = `-o ${options.outputPath}${process.platform === 'win32' ? '.exe' : ''}`
+  const cwd       = `${options.cwd}`
 
-  return runGoCommand(context, 'build', [output, mainFile])
+  return runGoCommand(context, 'build', [output, mainFile, cwd])
 }
